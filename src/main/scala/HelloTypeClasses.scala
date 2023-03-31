cf78b15712b246ac133ad5a13ced4a31b198d552
Lev Semin <semin.lev@gmail.com>
2023-03-31 12:31:25 +0300

    Use higher-kinded types for combine function

--- src/main/scala/HelloTypeClasses.scala
+++ src/main/scala/HelloTypeClasses.scala
@@ -16,7 +16,17 @@ given [A: Monoid, B: Monoid]: Monoid[(A, B)] with
   def empty: (A, B) = (summon[Monoid[A]].empty, summon[Monoid[B]].empty)
   def combine(a: (A, B), b: (A, B)): (A, B) = (summon[Monoid[A]].combine(a._1, b._1), summon[Monoid[B]].combine(a._2, b._2))
 
-def combine[A : Monoid](l: List[A]): A = l.foldRight(summon[Monoid[A]].empty)((a, b) => summon[Monoid[A]].combine(a, b))
+trait Foldable[F[_]] {
+  def foldRight[A](c: F[A], z: A)(op: (A, A) => A): A
+}
+
+given Foldable[List] with
+  def foldRight[A](c: List[A], z: A)(op: (A, A) => A): A = c.foldRight(z)(op)
+
+given Foldable[Set] with
+  def foldRight[A](c: Set[A], z: A)(op: (A, A) => A): A = c.foldRight(z)(op)
+
+def combine[A : Monoid, F[_]: Foldable](l: F[A]): A = summon[Foldable[F]].foldRight(l, summon[Monoid[A]].empty)((a, b) => summon[Monoid[A]].combine(a, b))
 
 object HelloTypeClasses extends IOApp.Simple {
   val run: IO[Unit] =
@@ -24,5 +34,6 @@ object HelloTypeClasses extends IOApp.Simple {
       _ <- IO(println(combine(List("1", "2", "3"))))
       _ <- IO(println(combine(List(1, 2, 3))))
       _ <- IO(println(combine(List((1, "1"), (2, "2"), (3, "3")))))
+      _ <- IO(println(combine(Set(3, 4, 5))))
     } yield ()
 }
